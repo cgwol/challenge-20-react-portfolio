474249ec126fe5b99222e4aa4d290b9d5945e393
cgwol <cgwalterson@icloud.com>
2023-11-13 01:18:56 -0700

    added resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,12 @@
+
 export default function Resume() {
-    return (
-      <span>Hello</span>
-    );
-  }
+  return (
+    <div className="container d-flex vh-100 justify-content-center text-center">
+      <iframe 
+        className="my-5" 
+        src="../images/resume.pdf" style={{ width: '100%', height: '100%' }}>
+
+      </iframe>
+    </div >
+  );
+}
